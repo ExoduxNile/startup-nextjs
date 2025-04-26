85596b8b085cb7a47802f9ade93272aac5b99550
exoduxnile <sandeep.tete2012@gmail.com>
2025-04-26 14:19:09 +0530

    iconimage

--- src/types/feature.ts
+++ src/types/feature.ts
@@ -1,9 +1,8 @@
 import type { JSX } from "react";
 import StaticImport from "next/image";
-import icon1 from "@/public/images/icon/icon-02.svg";
 export type Feature = {
   id: number;
-  icon: icon1;
+  icon: "/public/images/icon/icon-02.svg";
   title: string;
   description: string;
 };
