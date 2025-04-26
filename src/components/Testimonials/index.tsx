595c06083f20826b0c41b56d6db101f3b51147e5
exoduxnile <sandeep.tete2012@gmail.com>
2025-04-26 14:00:04 +0530

    testimonialsstar

--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -10,7 +10,7 @@ const testimonialData: Testimonial[] = [
     content:
       "Our members are so impressed. It's intuitive. It's clean. It's distraction free. If you're building a community.",
     image: "/images/testimonials/auth-01.png",
-    star: 5,
+
   },
   {
     id: 2,
@@ -19,7 +19,7 @@ const testimonialData: Testimonial[] = [
     content:
       "Our members are so impressed. It's intuitive. It's clean. It's distraction free. If you're building a community.",
     image: "/images/testimonials/auth-02.png",
-    star: 5,
+
   },
   {
     id: 3,
@@ -28,7 +28,7 @@ const testimonialData: Testimonial[] = [
     content:
       "Our members are so impressed. It's intuitive. It's clean. It's distraction free. If you're building a community.",
     image: "/images/testimonials/auth-03.png",
-    star: 5,
+
   },
 ];
 
