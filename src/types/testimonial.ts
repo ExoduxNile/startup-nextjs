698e28831a13ac992602819102f1c66ea717cda2
exoduxnile <sandeep.tete2012@gmail.com>
2025-04-26 14:10:54 +0530

    delete testi

--- src/types/testimonial.ts
+++
@@ -1,8 +0,0 @@
-import testi from "@public/images/testimonials/auth-02.png";
-export type Testimonial = {
-  id: number;
-  name: string;
-  designation: string;
-  content: string;
-  image: testi;
-};
