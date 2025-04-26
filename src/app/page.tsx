06e1e7a46a4d1411161daea25656097df0e0138b
exoduxnile <sandeep.tete2012@gmail.com>
2025-04-26 20:09:20 +0530

    InstagramScraper1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   return (
     <>
       {/* <ScrollUp /> */}
-      <Hero />
+      <InstagramScraper />
       {/* <Video />
       <Brands />
       <AboutSectionOne />
