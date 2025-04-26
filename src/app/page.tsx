187928ba944d711a7951419832b695f5ed416486
exoduxnile <sandeep.tete2012@gmail.com>
2025-04-26 16:15:32 +0530

    delete page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import AboutSectionTwo from "@/components/About/AboutSectionTwo";
 import Brands from "@/components/Brands";
 import ScrollUp from "@/components/Common/ScrollUp";
 import Contact from "@/components/Contact";
-import Features from "@/components/Features";
+
 import Hero from "@/components/Hero";
-import Pricing from "@/components/Pricing";
+
 import Video from "@/components/Video";
 import { Metadata } from "next";
 
@@ -20,12 +20,12 @@ export default function Home() {
     <>
       <ScrollUp />
       <Hero />
-      <Features />
+
       <Video />
       <Brands />
       <AboutSectionOne />
       <AboutSectionTwo />
-      <Pricing />
+
       <Contact />
     </>
   );
--- src/types/feature.ts
+++
@@ -1,8 +0,0 @@
-import type { JSX } from "react";
-import StaticImport from "next/image";
-export type Feature = {
-  id: number;
-  icon: "/images/icon/icon-02.svg";
-  title: string;
-  description: string;
-};
