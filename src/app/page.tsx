faf818ead03146715c0e29161973b31cf4630b01
exoduxnile <sandeep.tete2012@gmail.com>
2025-04-26 19:16:07 +0530

    hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import AboutSectionTwo from "@/components/About/AboutSectionTwo";
 import Brands from "@/components/Brands";
 import ScrollUp from "@/components/Common/ScrollUp";
 import Contact from "@/components/Contact";
-import InstagramScraper from "@/components/InstagramScraper";
+import Hero from "@/components/Hero";
 import Video from "@/components/Video";
 import { Metadata } from "next";
 
@@ -17,7 +17,7 @@ export default function Home() {
   return (
     <>
       {/* <ScrollUp /> */}
-      <InstagramScraper />
+      <Hero />
       {/* <Video />
       <Brands />
       <AboutSectionOne />
